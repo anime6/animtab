a8e85980c84412d45345d675f919bd3cda4967cd
thien <nguyenthanhthien.658jjh@gmail.com>
2021-02-02 16:11:16 +0700

    andoid

--- tabanimation/src/main/java/com/fukt/tabanimation/Value.java
+++ tabanimation/src/main/java/com/fukt/tabanimation/Value.java
@@ -1,12 +1,13 @@
 package com.fukt.tabanimation;
 
+
+
 import androidx.annotation.IntDef;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 public class Value {
-
 
     public static final int ANIMATION_STYLE_NONE = 0;
     public static final int ANIMATION_STYLE_EXTEND = 1;
